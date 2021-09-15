bb6d8599b109300bb3944d07c3109068af484fd3
castillc <90783266+castillc@users.noreply.github.com>
2021-09-15 11:27:55 -0400

    Added .py file P1LAB1_CastillaVirelles.py

---
+++ P1LAB1_CastillaVirelles.py
@@ -0,0 +1,9 @@
+#CTI 110
+#P1LAB1 - zylab training
+#Carlos Castilla Virelles
+#9/15/2021
+
+userNum = int(input())
+userNumSquared = userNum * userNum   # Bug here; fix it when instructed
+   
+print(userNumSquared)       # Output formatting issue here; fix it when instructed
