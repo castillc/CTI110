011da23a1c9734e5bb9c7dfcb9b22339408da13e
castillc <90783266+castillc@users.noreply.github.com>
2021-09-20 10:42:32 -0400

    Added P1T3 Assignment

---
+++ P1T3_CastillaVirelles.py
@@ -0,0 +1,19 @@
+#CTI 110
+#P1T3 - Movies
+#Carlos Castilla Virelles
+#9.20.2021
+
+#Program should print:
+#Movie name
+movieName = "City of God"
+print("Movie Name:", movieName)
+#Release Year
+releaseYear = (int(2002))
+print("Release Year:", releaseYear)
+#Box Office $
+boxOffice = (float(7.6))
+print ("Box Office $:", boxOffice, "million")
+#Quote
+movieQuote = """CARROT: You just killed the coolest hood in the City of God
+                                    (GUNSHOT)"""
+print (movieQuote)
